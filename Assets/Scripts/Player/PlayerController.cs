77a4a7c25ef00f16d7ed865ceb7ff98bc345c348
santy432 <71473265+santy432@users.noreply.github.com>
2026-05-14 19:21:56 -0300

    sfasfsfa

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,8 +6,6 @@ public class PlayerController : MonoBehaviourPun
 {
     [SerializeField] private float moveSpeed = 5f;
 
-    private bool canMove = false;
-
     private void Start()
     {
         if (photonView.IsMine)
@@ -18,7 +16,7 @@ public class PlayerController : MonoBehaviourPun
 
     private void Update()
     {
-        if (!photonView.IsMine || !canMove) return;
+        if (!photonView.IsMine) return;
 
         HandleMovement();
     }
@@ -35,25 +33,5 @@ public class PlayerController : MonoBehaviourPun
             transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
             Debug.Log("Moviendome: " + direction);
         }
-    }
-
-
-    [PunRPC]
-    private void RPC_StartRaceCountdown(double targetNetworkTime)
-    {
-        StartCoroutine(ExecuteCountdown(targetNetworkTime));
-    }
-
-    private IEnumerator ExecuteCountdown(double targetNetworkTime)
-    {
-        while (PhotonNetwork.Time < targetNetworkTime)
-        {
-            Debug.Log("Cuenta regresiva: " + (targetNetworkTime - PhotonNetwork.Time).ToString("F2") + " segundos restantes");
-        }
-
-        canMove = true;
-        // Aquí puedes iniciar la carrera
-        Debug.Log("¡Carrera iniciada!");
-        yield return null;
     }
 }
--- Assets/Scripts/Player/TankController.cs
+++ Assets/Scripts/Player/TankController.cs
@@ -1,4 +1,6 @@
 using Photon.Pun;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TankController : MonoBehaviourPun
@@ -7,6 +9,7 @@ public class TankController : MonoBehaviourPun
     [SerializeField] private float rotateSpeed = 90f;
 
     private Rigidbody rb;
+    private bool canMove = false;
 
     private void Awake()
     {
@@ -15,7 +18,7 @@ public class TankController : MonoBehaviourPun
 
     private void FixedUpdate()
     {
-        if (!photonView.IsMine || !rb) return;
+        if (!photonView.IsMine || !rb || !canMove) return;
 
         float move   = Input.GetAxis("Vertical");
         float rotate = Input.GetAxis("Horizontal");
@@ -24,5 +27,25 @@ public class TankController : MonoBehaviourPun
 
         Quaternion deltaRotation = Quaternion.Euler(Vector3.up * rotate * rotateSpeed * Time.fixedDeltaTime);
         rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+
+
+    [PunRPC]
+    private void RPC_StartRaceCountdown(double targetNetworkTime)
+    {
+        StartCoroutine(ExecuteCountdown(targetNetworkTime));
+    }
+
+    private IEnumerator ExecuteCountdown(double targetNetworkTime)
+    {
+        while (PhotonNetwork.Time < targetNetworkTime)
+        {
+            Debug.Log("Cuenta regresiva: " + (targetNetworkTime - PhotonNetwork.Time).ToString("F2") + " segundos restantes");
+        }
+
+        canMove = true;
+        // Aquí puedes iniciar la carrera
+        Debug.Log("¡Carrera iniciada!");
+        yield return null;
     }
 }
